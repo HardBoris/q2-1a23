2a93dab1f5f3808c60ee17cc57533572bb3e4102
HardBoris <bgaibor03@gmail.com>
2022-07-13 22:33:51 -0400

    feat: suma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
 export class App {
-  public message = 'Hello World!';
+  public message = "Preço Total:";
+
+  fruits = [
+    {
+      name: "banana",
+      color: "yellow",
+      price: 2,
+    },
+    {
+      name: "cherry",
+      color: "red",
+      price: 3,
+    },
+    {
+      name: "strawberry",
+      color: "red",
+      price: 4,
+    },
+  ];
+
+  suma = this.fruits.reduce((a, b) => a + b.price, 0);
 }
