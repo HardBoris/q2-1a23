ea41add8b1f25708d8da5a3f61cab6f8e98fd787
HardBoris <bgaibor03@gmail.com>
2022-07-18 07:46:15 -0400

    feat: file ok

--- src/app.ts
+++ src/app.ts
@@ -19,5 +19,28 @@ export class App {
     },
   ];
 
+  counter = 0;
+  lista = this.fruits;
   suma = this.fruits.reduce((a, b) => a + b.price, 0);
+
+  prueba() {
+    let lincoln = [];
+    lincoln = this.fruits.map((item) => item.color);
+
+    let filtrado = [];
+    filtrado = lincoln.filter((item, i) => lincoln.indexOf(item) === i);
+    if (this.counter === 3) {
+      this.counter = 0;
+    }
+    this.counter += 1;
+    let color = "";
+    color = filtrado[this.counter - 1];
+    if (color === undefined) {
+      this.lista = this.fruits;
+    } else {
+      this.lista = this.fruits.filter((item) => item.color === color);
+    }
+
+    this.suma = this.lista.reduce((a, b) => a + b.price, 0);
+  }
 }
